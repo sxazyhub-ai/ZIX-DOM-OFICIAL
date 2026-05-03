2f949dbc65ac6868c8fcc63f5f9bbcb19f8bc5bd
ZIX_HUB <sxazyhub@gmail.com>
2026-05-03 12:28:20 -0700

    Update ZIX.lua

--- ZIX.lua
+++ ZIX.lua
@@ -14,7 +14,7 @@ if not displayName or displayName == "" then
 end
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
-local title = ("ZIX Hub | Hello -%s"):format(displayName)
+local title = ("ZIX-DOM | Hello -%s"):format(displayName)
 
 local library = loadstring(game:HttpGet("https://pastebin.com/raw/wqJ8PvkW", true))()
 
